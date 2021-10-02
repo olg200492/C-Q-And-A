06e98b65528f282a38ac1788f875df3930f0bd9b
Olga L <olg200492@gmail.com>
2021-10-02 14:58:29 +0300

    new code samples

--- 2.c
+++ 2.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
-int cal(int sec);
-int main()
-{   
-    cal(4000);
-    return 0;
-    
-}
-int cal(int sec){
-    int h = sec/3600;
-    int m = (sec - h*3600)/60;
-    int s= (sec - h*3600)%60;
-    printf("%02d:%02d:%02d",h,m,s);
-}
+#include <string.h>
+#include <math.h>
+ /* function declaration */
+ void StrCopy(char *str1, char *str2);
+ /* main() function */
+ int main()
+ {
+    int numIntegers,i,sum=0;
+    int *ptr;
+    printf("How many bytes?\n");
+    scanf("%d",&numIntegers);
+
+    int termination = 0;
+
+    ptr = malloc(numIntegers * sizeof(int));
+    if (ptr == NULL){
+      printf("malloc() failed.\n");
+      termination = 1;
+    }
+    else{
+      for(i=0;i<numIntegers;i++)
+        ptr[i]=i+1;
+      for (i=0; i<numIntegers; i++)
+        sum += ptr[i];
+      printf("The sum is %d.\n", sum);
+    }
+    free(ptr);
+    return termination;
+ }
