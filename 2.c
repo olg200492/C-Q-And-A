d55d4c8d1f27bd25c453f0d4c3d760c154aa607d
Olga L <olg200492@gmail.com>
2021-10-03 16:45:32 +0300

    workshop 19.3

--- 2.c
+++ 2.c
@@ -1,24 +1,38 @@
+ /* 19L04.c Pointing to a structure */
  #include <stdio.h>
- struct employee {
-       int id;
-       char name[32];
-       };
-    /* structure initialization */
-    
-void printStructEmployee(struct employee emp);
+struct computer {
+   float cost;
+   int year;
+   int cpu_speed;
+   char cpu_type[16];
+};
+
+ typedef struct computer SC;
+
+ void DataReceive(SC *ptr_s);
+
  int main(void)
  {
-    struct employee info = {
-       1,
-       "B. Smith"
-       };
+    SC model;
 
-    printStructEmployee(info);
+    DataReceive(&model);
+    printf("Here are what you entered:\n");
+    printf("Year: %d\n", model.year);
+    printf("Cost: $%6.2f\n", model.cost);
+    printf("CPU type: %s\n", model.cpu_type);
+    printf("CPU speed: %d MHz\n", model.cpu_speed);
 
     return 0;
  }
- void printStructEmployee(struct employee emp){
-  printf("Here is a sample:\n");
-  printf("Employee Name: %s\n", emp.name);
-  printf("Employee ID #: %04d\n\n", emp.id);
+ /* function definition */
+ void DataReceive(SC *ptr_s)
+ {
+    printf("The type of the CPU inside your computer?\n");
+       gets(ptr_s->cpu_type);
+    printf("The speed(MHz) of the CPU?\n");
+       scanf("%d", &ptr_s->cpu_speed);
+    printf("The year your computer was made?\n");
+       scanf("%d", &ptr_s->year);
+    printf("How much you paid for the computer?\n");
+       scanf("%f", &ptr_s->cost);
  }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-termination
+model
